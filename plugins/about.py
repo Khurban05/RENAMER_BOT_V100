c383cda1acd5c7a4c6c30462112ab4ee57f87bbf
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2024-01-25 20:54:06 +0500

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -4,10 +4,10 @@ token = os.environ.get('TOKEN','')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
-@Client.on_message(filters.private & filters.command(["about"]))
+@Client.on_message(filters.private & filters.command(["status"]))
 async def start(client,message):
 	botdata(int(botid))
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Umumiy qayta nomlangan fayllar: {total_rename}ta\nUmumiy qayta nomlangan fayllar hajmi: {humanbytes(int(total_size))} ",quote=True)
