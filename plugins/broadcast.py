ffaf10c037072c5470c460e2be795e1103e8a444
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2024-01-28 20:20:30 +0500

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,7 +4,9 @@ import asyncio
 from pyrogram import Client ,filters
 from helper.database import getid ,delete
 import time
-ADMIN = int(os.environ.get("ADMIN", 5415124528))
+ADMIN = int(os.environ.get("ADMIN", 795726700))
+ 
+
  
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
