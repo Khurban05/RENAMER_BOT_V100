f24e6eca82cca0de1a641496f8f9f01942c93a6c
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2024-01-28 20:21:26 +0500

    Update thumbfunction.py

--- plugins/thumbfunction.py
+++ plugins/thumbfunction.py
@@ -1,6 +1,9 @@
 from pyrogram import Client, filters
 from helper.database import find, delthumb, addthumb
 
+
+
+
 @Client.on_message(filters.private & filters.command(['thumbnail']))
 async def viewthumb(client,message):
 		print(message.chat.id)
@@ -8,16 +11,16 @@ async def viewthumb(client,message):
 		if thumb :
 			await client.send_photo(message.chat.id,photo =f"{thumb}")
 		else:
-			await message.reply_text("**Sizda hech qanday pechat rasm yo'q!!!\n\nPechat rasm qo'yish uchun shunchaki rasm yuboring.**")
+			await message.reply_text("<b>Sizda hech qanday pechat rasm yo'q!!!\n\nPechat rasm qo'yish uchun shunchaki rasm yuboring.</b>")
 	
 	
 @Client.on_message(filters.private & filters.command(['del_thumbnail']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
-	await message.reply_text("**Maxsus pechat rasm muvaffaqiyatli oʻchirildi✅**")
+	await message.reply_text("<b>Maxsus pechat rasm muvaffaqiyatli oʻchirildi✅</b>")
 
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
 	file_id = str(message.photo.file_id)
 	addthumb(message.chat.id , file_id)
-	await message.reply_text("**Maxsus pechat rasm muvaffaqiyatli saqlandi✅**")
+	await message.reply_text("<b>Maxsus pechat rasm muvaffaqiyatli saqlandi✅</b>")
