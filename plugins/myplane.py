34c343b3b46c06be9e6d7751f6106218b9d66389
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2024-02-25 11:20:26 +0500

    Update myplane.py

--- plugins/myplane.py
+++ plugins/myplane.py
@@ -7,39 +7,32 @@ import datetime
 from datetime import timedelta, date ,datetime
 from datetime import date as date_
 from helper.progress import humanbytes
-from helper.database import daily as daily_
-from helper.date import check_expi
-from helper.database import uploadlimit , usertype,backpre
 
-@Client.on_message(filters.private & filters.command(["myplan"]))
+
+@Client.on_message(filters.private & filters.command(["tarif"]))
 async def start(client,message):
-	used_ = find_one(message.from_user.id)	
-	daily = used_["daily"]
-	expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
-	if expi != 0:
-	     today = date_.today()
-	     pattern = '%Y-%m-%d'
-	     epcho = int(time.mktime(time.strptime(str(today), pattern)))
-	     daily_(message.from_user.id,epcho)
-	     used_limit(message.from_user.id,0)
-	_newus = find_one(message.from_user.id)
-	used = _newus["used_limit"]
-	limit = _newus["uploadlimit"]
-	remain = int(limit)- int(used)
-	user =  _newus["usertype"]
-	ends = _newus["prexdate"]
-	if ends:
-	    pre_check = check_expi(ends)
-	    if pre_check == False:
-	        backpre(message.from_user.id)
-	if ends == None:
-	    text = f"User ID:- ```{message.from_user.id}```\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}"
-	else:
-	    normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
-	    text = f"User ID:- ```{message.from_user.id}```\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}\n\n```Your Plan Ends On :- {normal_date}"
-	    
-	if user == "Free":
-	    await message.reply(text,quote = True,reply_markup = InlineKeyboardMarkup([[       			InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade"), InlineKeyboardButton("Cancel ✖️ ",callback_data = "cancel") ]]))
-	else:
-	    await message.reply(text,quote=True)
-	    
+        used_ = find_one(message.from_user.id)        
+        daily = used_["daily"]
+        expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
+        if expi != 0:
+             today = date_.today()
+             pattern = '%Y-%m-%d'
+             epcho = int(time.mktime(time.strptime(str(today), pattern)))
+             daily_(message.from_user.id,epcho)
+             used_limit(message.from_user.id,0)
+        _newus = find_one(message.from_user.id)
+        used = _newus["used_limit"]
+        limit = _newus["uploadlimit"]
+        remain = int(limit)- int(used)
+        user =  _newus["usertype"]
+        ends = _newus["prexdate"]
+        if ends == None:
+            text = f"Foydalanuvchi ID: ```{message.from_user.id}```\nTarif: {user}\nKunlik yuklash limiti: {humanbytes(limit)}\nBugun foydalanilgan: {humanbytes(used)}\nQolgan hajm: {humanbytes(remain)}"
+        else:
+            normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
+            text = f"Foydalanuvchi ID: ```{message.from_user.id}```\nTa'rif : {user}\nKunlik yuklash limiti: {humanbytes(limit)}\nBugun foydalanilgan: {humanbytes(used)}\nQolgan hajm: {humanbytes(remain)}\n\n```Sizning ta'rifingiz tugaydi: {normal_date} da"
+
+        if user == "Bepul":
+            await message.reply(text,quote = True,reply_markup = InlineKeyboardMarkup([[                               InlineKeyboardButton("Bekor qilish ✖️ ",callback_data = "cancel") ]]))
+        else:
+            await message.reply(text,quote=True)
