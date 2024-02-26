a29b9c19a8260ee749b0358e7317c824b5608dd1
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2024-02-26 16:58:56 +0500

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -6,14 +6,13 @@ from pyrogram import Client , filters
 async def upgrade(bot,update):
         text = """Tariflar✅
 
-Bepul                |Mini💫
-Kunlik limit: 100MB  |Kunlik limit: 5GB
-Narxi: Bepul.        |Narxi: 1.5$/oy
+Bepul                       | Mini💫
+Kunlik limit: 100MB  | Kunlik limit: 5GB
+Narxi: Bepul.             |vNarxi: 1.5💲/oy
 
-Lite✨              |VIP⚡
-Kunlik limit: 15GB  |Kunlik limit: 50GB
-Narxi: 4$/oy        |Narxi: 7$/oy
-
+Lite✨                   | VIP⚡
+Kunlik limit: 15GB  | Kunlik limit: 50GB
+Narxi: 4💲/oy           | Narxi: 7💲/oy
 
 Premium 👑             
 Kunlik limit: 150GB 
@@ -21,8 +20,8 @@ Narxi: 15💲/oy
  
 To'lov usullari: HUMO💳  | UZCARD💳 |  VISA💳
  
-Ta'rif sotib olmoqchi bo'lsangiz admin bilan bog'laning.
-4Gbgacha faylni yuklash uchun Telegram Premium va undan tashqari bot ishlashi uchun server kerak. Bizni to'g'ri tushundingiz degan umiddamiz🙂"""
+  Ta'rif sotib olmoqchi bo'lsangiz admin bilan bog'laning.
+  4Gbgacha faylni yuklash uchun Telegram Premium va undan tashqari bot ishlashi uchun server kerak. Bizni to'g'ri tushundingiz degan umiddamiz🙂"""
         keybord = InlineKeyboardMarkup([[ 
                                 InlineKeyboardButton("Administrator",url = "https://t.me/Coder_MYP")], 
                                 [InlineKeyboardButton("Bekor qilish",callback_data = "cancel")  ]])
@@ -33,13 +32,13 @@ Ta'rif sotib olmoqchi bo'lsangiz admin bilan bog'laning.
 async def upgradecm(bot,message):
         text = """Tariflar✅
 
-Bepul                |Mini💫
-Kunlik limit: 100MB  |Kunlik limit: 5GB
-Narxi: Bepul.        |Narxi: 1.5$/oy
+Bepul                       | Mini💫
+Kunlik limit: 100MB  | Kunlik limit: 5GB
+Narxi: Bepul.             | Narxi: 1.5💲/oy
 
-Lite✨              |VIP⚡
-Kunlik limit: 15GB  |Kunlik limit: 50GB
-Narxi: 4$/oy        |Narxi: 7$/oy
+Lite✨                   | VIP⚡
+Kunlik limit: 15GB  | Kunlik limit: 50GB
+Narxi: 4💲/oy           | Narxi: 7💲/oy
 
 Premium 👑             
 Kunlik limit: 150GB 
@@ -47,8 +46,8 @@ Narxi: 15💲/oy
  
 To'lov usullari: HUMO💳  | UZCARD💳 |  VISA💳
  
-Ta'rif sotib olmoqchi bo'lsangiz admin bilan bog'laning.
-4Gbgacha faylni yuklash uchun Telegram Premium va undan tashqari bot ishlashi uchun server kerak. Bizni to'g'ri tushundingiz degan umiddamiz🙂"""
+  Ta'rif sotib olmoqchi bo'lsangiz admin bilan bog'laning.
+  4Gbgacha faylni yuklash uchun Telegram Premium va undan tashqari bot ishlashi uchun server kerak. Bizni to'g'ri tushundingiz degan umiddamiz🙂"""
         keybord = InlineKeyboardMarkup([[ 
                                 InlineKeyboardButton("Administrator",url = "https://t.me/Coder_MYP")],                   [InlineKeyboardButton("VISA💳",url = "https://t.me/Coder_MYP")],[InlineKeyboardButton("Bekor qilish",callback_data = "cancel")  ]])
         await message.reply_text(text = text,reply_markup = keybord)
